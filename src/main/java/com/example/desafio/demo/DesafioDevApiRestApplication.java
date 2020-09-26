e533d01ff803c3c0716a27ec739892bbf53f2783
Julia Vaz <juliavaz.dev@gmail.com>
2020-09-26 14:31:19 -0300

    adicionando dependencias

---
+++ src/main/java/com/example/desafio/demo/DesafioDevApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio/demo/DesafioDevApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDevApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
